152f4153184341b2101dada4abe8ca55632f7e0d
humanperson2024 <hunteronin@gmail.com>
2025-10-02 14:05:49 -0700

    hello

---
+++ test.js
@@ -0,0 +1,11 @@
+const { text } = require("express");
+
+
+let num = -100 
+ 
+if (num >= 10) { 
+  
+  console.log("Your number is large enough"); 
+} else { 
+  console.log("Your number is too small"); 
+} 
