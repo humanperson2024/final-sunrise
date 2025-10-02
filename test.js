6164bfda5e1a3d107c9910da82e0d064675acf2a
humanperson2024 <hunteronin@gmail.com>
2025-10-02 14:36:46 -0700

    n

--- test.js
+++ test.js
@@ -1,11 +1,23 @@
 const { text } = require("express");
 
+function myFunction() {
+    
+var x = document.getElementById("myText").value;
+document.getElementById("demo").innerHTML = x;
 
-let num = -100 
- 
+if (num >= 10) { 
+
+    console.log("Your number is large enough"); 
+} else { 
+    console.log("Your number is too small"); 
+} 
+}
+
+let num = ("demo") 
+
 if (num >= 10) { 
   
-  console.log("Your number is large enough"); 
+    console.log("Your number is large enough"); 
 } else { 
-  console.log("Your number is too small"); 
+    console.log("Your number is too small"); 
 } 
